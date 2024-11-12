dcb819ea87c0e1f86c47704d15af17f437bab9a0
NevenDog <mattiacimo777@gmail.com>
2024-11-12 12:21:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-camera.position.z = 30;
+camera.position.z = 10;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
